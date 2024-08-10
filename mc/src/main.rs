d32695199aa6aba28804cbbefcff91a44a5f49f6
surajsharma <suraj@evenzero.in>
2024-08-10 11:38:47 +0530

    fix: improved mc

--- mc/src/main.rs
+++ mc/src/main.rs
@@ -2,6 +2,9 @@ use std::env;
 use std::path::Path;
 use std::process::Command;
 use std::process::ExitCode;
+use std::process::Output;
+
+use colored::*;
 
 struct Flags {
     lex: bool,
@@ -13,82 +16,62 @@ struct Flags {
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
 
-    let mut flags = Flags {
-        lex: false,
-        parse: false,
-        codegen: false,
-        help: false,
-    };
-
-    if args.len() < 2 {
-        println!(
-            "Usage: {} <file_name> [--lex] [--parse] [--codegen]",
-            args[0]
-        );
-
-        return ExitCode::from(1);
-    }
-
-    // Process command line arguments
-    for arg in &args[1..] {
-        match arg.as_str() {
-            "--lex" | "-L" => flags.lex = true,
-            "--parse" | "-P" => flags.parse = true,
-            "--codegen" | "-C" => flags.codegen = true,
-            "--help" | "-h" => flags.help = true,
-            _ => {}
+    match process_args(&args) {
+        Err(err) => {
+            println!("\n{} {}", "Error: ".red().bold(), err);
+            return ExitCode::FAILURE;
         }
-    }
 
-    if flags.help {
-        print_usage();
-        return ExitCode::from(0);
-    }
-
-    let file_name = &args[1];
-    let path = Path::new(file_name);
-
-    if !path.is_file() {
-        println!("{} is invalid file! see usage with --help or -h", file_name);
-        return ExitCode::from(1);
-    }
-
-    let processed_file = add_processed_to_filename(file_name);
-
-    // Execute the gcc command
-    let output = Command::new("gcc")
-        .arg("-E")
-        .arg("-P")
-        .arg(file_name)
-        .arg("-o")
-        .arg(&processed_file)
-        .output();
-
-    match output {
-        Ok(output) => {
-            if output.status.success() {
-                println!("gcc preprocessor ran successfully.\n");
-                if flags.lex {
-                    println!("lexer will run\n");
-                }
-
-                if flags.parse {
-                    println!("parser will run\n");
-                }
-
-                if flags.codegen {
-                    println!("codegen will run\n");
-                }
-                ExitCode::from(0)
-            } else {
-                println!("gcc preprocessor failed to execute.\n");
-                println!("Error: {}", String::from_utf8_lossy(&output.stderr));
-                ExitCode::from(1)
+        Ok(flags) => {
+            if flags.help {
+                print_usage();
+                return ExitCode::SUCCESS;
             }
-        }
-        Err(e) => {
-            println!("Failed to execute gcc preprocessor: {}\n", e);
-            ExitCode::from(1)
+
+            let file = &args[1];
+            let processed_file = get_processed_filename(file);
+
+            // Execute the gcc preprocessor command
+            match run_preprocessor(&file, &processed_file) {
+                Ok(output) => {
+                    if output.status.success() {
+                        println!(
+                            "\n{} gcc preprocessor ran successfully.\n",
+                            "Success:".green().bold()
+                        );
+
+                        if flags.lex {
+                            run_lexer();
+                        }
+
+                        if flags.parse {
+                            run_parser();
+                        }
+
+                        if flags.codegen {
+                            run_codegen();
+                        }
+
+                        return ExitCode::SUCCESS;
+                    } else {
+                        println!(
+                            "\n{} {}",
+                            "Error:".red().bold(),
+                            String::from_utf8_lossy(&output.stderr)
+                        );
+                        return ExitCode::FAILURE;
+                    }
+                }
+                Err(e) => {
+                    println!(
+                        "\n{} Failed to execute gcc preprocessor: {}\n{}\n",
+                        "Error: ".red().bold(),
+                        e,
+                        "Is gcc installed and in $PATH?".yellow().bold()
+                    );
+                    return ExitCode::FAILURE;
+                }
+            }
         }
     }
 }
@@ -97,22 +80,114 @@ fn main() -> ExitCode {
     You’ll have to stub out this step, since you haven’t written your compiler yet.
     Delete the preprocessed file when you’re done with it.
 */
+
 /* STEP 3 - Assemble and link the assembly file to produce an executable, using
     gcc ASSEMBLY_FILE -o OUTPUT_FILE
     Delete the assembly file when you’re done with it.
 */
 
-fn add_processed_to_filename(file_name: &str) -> String {
+fn run_lexer() {
+    println!("✅ lexer will run");
+}
+
+fn run_parser() {
+    println!("✅ parser will run");
+}
+
+fn run_codegen() {
+    println!("✅ codegen will run");
+}
+
+fn run_preprocessor(file: &str, processed_file: &str) -> Result<Output, std::io::Error> {
+    return Command::new("gcc")
+        .arg("-E")
+        .arg("-P")
+        .arg(file)
+        .arg("-o")
+        .arg(processed_file)
+        .output();
+}
+
+fn process_args(args: &[String]) -> Result<Flags, String> {
+    let mut flags = Flags {
+        lex: false,
+        parse: false,
+        codegen: false,
+        help: false,
+    };
+
+    if args.len() < 2 {
+        return Err(format!(
+            "{} {} <file_name> [--lex] [--parse] [--codegen]",
+            "Usage: ".green().bold(),
+            args[0]
+        ));
+    }
+
+    let mut i = 2; // Skip the program name and file name
+    let file = &args[1];
+    let path = Path::new(file);
+
+    if !path.is_file() {
+        return Err(format!(
+            "invalid file: '{}'. Try {} or {} for more information.",
+            file,
+            "--help".cyan().bold(),
+            "-h".cyan().bold()
+        ));
+    }
+
+    while i < args.len() {
+        match args[i].as_str() {
+            "--lex" | "-L" => flags.lex = true,
+            "--parse" | "-P" => flags.parse = true,
+            "--codegen" | "-C" => flags.codegen = true,
+            "--help" | "-h" => flags.help = true,
+            _ => {
+                return Err(format!(
+                    "invalid argument: '{}'. Try {} or {} for more information.",
+                    args[i],
+                    "--help".cyan().bold(),
+                    "-h".cyan().bold()
+                ));
+            }
+        }
+        i += 1;
+    }
+
+    Ok(flags)
+}
+
+fn get_processed_filename(file_name: &str) -> String {
     let path = Path::new(file_name);
     let stem = path.file_stem().and_then(|s| s.to_str()).unwrap_or("");
     format!("{}_preprocessed.{}", stem, "i")
 }
 
 fn print_usage() {
-    println!("Usage: program <input_file> [options]");
-    println!("Options:");
-    println!("  --lex, -L       Perform lexical analysis");
-    println!("  --parse, -P     Perform parsing");
-    println!("  --codegen, -C   Perform code generation");
-    println!("  --help, -h      Display this help message");
+    println!(
+        "\n{} program <input_file> [options]",
+        "Usage: ".green().bold()
+    );
+    println!("{}", "Options:".cyan().bold());
+    println!(
+        "{},\t\t{}\tPerform lexical analysis",
+        "--lex".cyan().bold(),
+        "-L".cyan()
+    );
+    println!(
+        "{},\t{}\tPerform parsing",
+        "--parse".cyan().bold(),
+        "-P".cyan()
+    );
+    println!(
+        "{},\t{}\tPerform code generation",
+        "--codegen".cyan().bold(),
+        "-C".cyan()
+    );
+    println!(
+        "{},\t\t{}\tDisplay this help message",
+        "--help".cyan().bold(),
+        "-h".cyan()
+    );
 }
